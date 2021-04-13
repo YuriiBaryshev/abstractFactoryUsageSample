6cb9ee603a358326aa4feeb05f59db9e8708148e
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-13 12:38:42 +0300

    fingerprint reader was implemeted

---
+++ classes/fingerprintReader.js
@@ -0,0 +1,11 @@
+let FactorReader = require('factorReader.js')
+
+class FingerprintReader extends FactorReader {
+  function readAuthParameters() {
+    //
+    let fingerprint = [[0x11, 0x22, 0x33],
+                      [0x44, 0x55, 0x66]
+                      [0x77, 0x88, 0x99]];
+    return fingerprint;
+  }
+}
