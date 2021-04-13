67c748976003007ee71e53a49d2bf56b8d02dde1
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-13 12:38:05 +0300

    calling prevention was added

--- classes/factorReader.js
+++ classes/factorReader.js
@@ -1,3 +1,7 @@
 class FactorReader {
-  function readAuthParameters();
+  function readAuthParameters() {
+    throw Error("You shouldn't call methods from 'abstract' class");
+  }
 }
+
+module.exports = FactorReader
