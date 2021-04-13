9a60caf565ad3042cb96d33537c1e33ca71e634e
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-13 12:48:54 +0300

    auth type was created and its setter/getter

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-class AbstractFactory {
 
+let authenticationType = {
+  PASSWORD : "password",
+  FINGERPRINT : "fingerprint",
+}
+
+class AbstractFactory {
+  let _authType;
+
+  constructor() {
+    _authType = authenticationType.PASSWORD;
+  }
+
+  setter authType(newType) {
+    switch (newType) {
+      case authenticationType.PASSWORD:
+        _authType = authenticationType.PASSWORD
+        break;
+      case authenticationType.FINGERPRINT:
+        _authType = authenticationType.FINGERPRINT
+        break;
+      default:
+        throw new Error("Unsupported authentication type");
+    }
+
+  getter authType() {
+    return _authType;
+  }
 }
 
 module.exports = AbstractFactory
