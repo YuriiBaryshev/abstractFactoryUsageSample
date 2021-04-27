b622c02019c799d5cc075d00c2fb35e31e346626
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-27 11:01:54 +0300

    FingerprintAuthenticator was added

---
+++ classes/fingerprintAuthentificator.js
@@ -0,0 +1,25 @@
+let Authenticator = require('authenticator.js')
+
+class FingerprintAuthenticator extends Authenticator {
+  _idealValue;
+
+  constructor() {
+    _idealValue = [[0x11, 0x22, 0x33],
+                  [0x44, 0x55, 0x66]
+                  [0x77, 0x88, 0x99]];
+  }
+
+  function authenticate(authRawData) {
+    let isIdentical = true;
+    for (let i = 0; i < _idealValue.length; i++) {
+      for (let j = 0; j < _idealValue[i].length; j++) {
+        isIdentical = isIdentical && (_idealValue[i][j] == authRawData[i][j]);
+      }
+    }
+    return isIdentical;
+  }
+
+  function changeSource(newIdealValue) {
+    _idealValue = newIdealValue;
+  }
+}
