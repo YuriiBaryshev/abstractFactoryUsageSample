7b7c16d50df2cf9972ff5f64920a5524cf428f66
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-27 11:47:39 +0300

    differrent instances creation tests were added

--- test/test.js
+++ test/test.js
@@ -8,4 +8,21 @@ describe('Abstract Fabric pattern demonstration', function() {
     assert.equal(authPair.reader != {}, true);
     assert.equal(authPair.authenticator != {}, true);
   });
+
+  it('create password-related auth pair by default', function() {
+    let authFactory = new AbstractFactory();
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    assert.isAtLeast(authFactor.length, 4);
+    assert.equal(authFactor[0].length, 1);
+  });
+
+  it('create fingerprint-related auth pair', function() {
+    let authFactory = new AbstractFactory();
+    authFactory.authType = 'fingerprint';
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    assert.equal(authFactor.length, 3);
+    assert.equal(authFactor[0].length, 3);
+  });
 });
