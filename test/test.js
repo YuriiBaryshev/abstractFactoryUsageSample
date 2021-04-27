af9b5238305eddd1bb55f7bf018344c3e0ac7cca
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-27 12:00:12 +0300

    negative tests were added

--- test/test.js
+++ test/test.js
@@ -25,4 +25,38 @@ describe('Abstract Fabric pattern demonstration', function() {
     assert.equal(authFactor.length, 3);
     assert.equal(authFactor[0].length, 3);
   });
+
+  it('passes password-related default instances authentication', function() {
+    let authFactory = new AbstractFactory();
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    assert.equal(authPair.authenticator.authenticate(authFactor), true);
+  });
+
+  it('passes fingerprint-related default instances authentication', function() {
+    let authFactory = new AbstractFactory();
+    authFactory.authType = 'fingerprint';
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    assert.equal(authPair.authenticator.authenticate(authFactor), true);
+  });
+
+  it('fails password-related altered instances authentication', function() {
+    let authFactory = new AbstractFactory();
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    authPair.authenticator.changeSource("0987654321");
+    assert.notEqual(authPair.authenticator.authenticate(authFactor), true);
+  });
+
+  it('fails fingerprint-related altered instances authentication', function() {
+    let authFactory = new AbstractFactory();
+    authFactory.authType = 'fingerprint';
+    let authPair = authFactory.createAuthObjects();
+    let authFactor = authPair.reader.readAuthParameters();
+    authPair.authenticator.changeSource([[0x99, 0x88, 0x77],
+                                        [0x66, 0x55, 0x44],
+                                        [0x33, 0x22, 0x11]]);
+    assert.notEqual(authPair.authenticator.authenticate(authFactor), true);
+  });
 });
