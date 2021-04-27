ae431f5d839d3a6f9bef11eb0690752fc896f135
yuriibaryshev <yura.baryshev@gmail.com>
2021-04-27 10:55:42 +0300

    PasswordAuthenticator was added

---
+++ classes/passwordAuthentificator.js
@@ -0,0 +1,17 @@
+let Authenticator = require('authenticator.js')
+
+class PasswordAuthenticator extends Authenticator {
+  _idealValue;
+
+  constructor() {
+    _idealValue = '1234567890';
+  }
+
+  function authenticate(authRawData) {
+    return _idealValue == authRawData;
+  }
+
+  function changeSource(newIdealValue) {
+    _idealValue = newIdealValue;
+  }
+}
